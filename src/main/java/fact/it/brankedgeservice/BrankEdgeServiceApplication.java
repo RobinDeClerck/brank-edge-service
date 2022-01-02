961e0472fa295f9f07f038d2358b8154f07bae64
Robin De Clerck <robin.de.clerck@gmail.com>
2022-01-02 23:50:03 +0100

    Added getRestTemplate bean

--- src/main/java/fact/it/brankedgeservice/BrankEdgeServiceApplication.java
+++ src/main/java/fact/it/brankedgeservice/BrankEdgeServiceApplication.java
@@ -2,9 +2,16 @@ package fact.it.brankedgeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class BrankEdgeServiceApplication {
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BrankEdgeServiceApplication.class, args);
