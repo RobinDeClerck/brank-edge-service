b88569f6bc94bf294ede131ac7f9e5142972923d
Robin De Clerck <robin.de.clerck@gmail.com>
2022-01-02 19:38:29 +0100

    Initial commit

---
+++ src/main/java/fact/it/brankedgeservice/BrankEdgeServiceApplication.java
@@ -0,0 +1,13 @@
+package fact.it.brankedgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrankEdgeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrankEdgeServiceApplication.class, args);
+    }
+
+}
