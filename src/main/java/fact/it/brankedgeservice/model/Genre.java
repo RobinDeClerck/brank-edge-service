ffc339801b8e5866b2d0be3602614566fabcf1ee
Robin De Clerck <robin.de.clerck@gmail.com>
2022-01-07 21:14:04 +0100

    Removed id null

--- src/main/java/fact/it/brankedgeservice/model/Genre.java
+++ src/main/java/fact/it/brankedgeservice/model/Genre.java
@@ -2,7 +2,6 @@ package fact.it.brankedgeservice.model;
 
 
 public class Genre {
-    private String id;
     private String genreName;
     private String description;
 
@@ -12,14 +11,6 @@ public class Genre {
     public Genre(String genreName, String description) {
         this.genreName = genreName;
         this.description = description;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 
     public String getGenreName() {
